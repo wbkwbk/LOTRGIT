a22dde386ef1c4020fab8a5283678dfc95c6da01
wbkwbk <80103165+wbkwbk@users.noreply.github.com>
2024-11-27 20:30:08 +0100

    3 led stripes in parallel

--- Production/Tests/LEDTest/LEDController.h
+++ Production/Tests/LEDTest/LEDController.h
@@ -1,3 +1,75 @@
+/*
+
+#define FX_MODE_STATIC                   0
+#define FX_MODE_BLINK                    1
+#define FX_MODE_BREATH                   2
+#define FX_MODE_COLOR_WIPE               3
+#define FX_MODE_COLOR_WIPE_INV           4 
+#define FX_MODE_COLOR_WIPE_REV           5
+#define FX_MODE_COLOR_WIPE_REV_INV       6
+#define FX_MODE_COLOR_WIPE_RANDOM        7
+#define FX_MODE_RANDOM_COLOR             8
+#define FX_MODE_SINGLE_DYNAMIC           9
+#define FX_MODE_MULTI_DYNAMIC           10
+#define FX_MODE_RAINBOW                 11
+#define FX_MODE_RAINBOW_CYCLE           12
+#define FX_MODE_SCAN                    13
+#define FX_MODE_DUAL_SCAN               14
+#define FX_MODE_FADE                    15
+#define FX_MODE_THEATER_CHASE           16
+#define FX_MODE_THEATER_CHASE_RAINBOW   17
+#define FX_MODE_RUNNING_LIGHTS          18
+#define FX_MODE_TWINKLE                 19
+#define FX_MODE_TWINKLE_RANDOM          20
+#define FX_MODE_TWINKLE_FADE            21
+#define FX_MODE_TWINKLE_FADE_RANDOM     22
+#define FX_MODE_SPARKLE                 23
+#define FX_MODE_FLASH_SPARKLE           24
+#define FX_MODE_HYPER_SPARKLE           25
+#define FX_MODE_STROBE                  26
+#define FX_MODE_STROBE_RAINBOW          27
+#define FX_MODE_MULTI_STROBE            28
+#define FX_MODE_BLINK_RAINBOW           29
+#define FX_MODE_CHASE_WHITE             30
+#define FX_MODE_CHASE_COLOR             31
+#define FX_MODE_CHASE_RANDOM            32
+#define FX_MODE_CHASE_RAINBOW           33
+#define FX_MODE_CHASE_FLASH             34
+#define FX_MODE_CHASE_FLASH_RANDOM      35
+#define FX_MODE_CHASE_RAINBOW_WHITE     36
+#define FX_MODE_CHASE_BLACKOUT          37
+#define FX_MODE_CHASE_BLACKOUT_RAINBOW  38
+#define FX_MODE_COLOR_SWEEP_RANDOM      39
+#define FX_MODE_RUNNING_COLOR           40
+#define FX_MODE_RUNNING_RED_BLUE        41
+#define FX_MODE_RUNNING_RANDOM          42
+#define FX_MODE_LARSON_SCANNER          43
+#define FX_MODE_COMET                   44
+#define FX_MODE_FIREWORKS               45
+#define FX_MODE_FIREWORKS_RANDOM        46
+#define FX_MODE_MERRY_CHRISTMAS         47
+#define FX_MODE_FIRE_FLICKER            48
+#define FX_MODE_FIRE_FLICKER_SOFT       49
+#define FX_MODE_FIRE_FLICKER_INTENSE    50
+#define FX_MODE_CIRCUS_COMBUSTUS        51
+#define FX_MODE_HALLOWEEN               52
+#define FX_MODE_BICOLOR_CHASE           53
+#define FX_MODE_TRICOLOR_CHASE          54
+#define FX_MODE_TWINKLEFOX              55
+#define FX_MODE_RAIN                    56
+#define FX_MODE_CUSTOM                  57  // keep this for backward compatiblity
+#define FX_MODE_CUSTOM_0                57  // custom modes need to go at the end
+#define FX_MODE_CUSTOM_1                58
+#define FX_MODE_CUSTOM_2                59
+#define FX_MODE_CUSTOM_3                60
+#define FX_MODE_CUSTOM_4                61
+#define FX_MODE_CUSTOM_5                62
+#define FX_MODE_CUSTOM_6                63
+#define FX_MODE_CUSTOM_7                64
+
+*/
+
+
 #ifndef LEDCONTROLLER_H
 #define LEDCONTROLLER_H
 
--- Production/Tests/LEDTest/LEDTest.ino
+++ Production/Tests/LEDTest/LEDTest.ino
@@ -1,15 +1,29 @@
 #include "LEDController.h"
 
-#define LED_COUNT 60
-#define LED_PIN 7
-                                                                       //brightness, speed, duration in millis 
-LEDController ledController(LED_COUNT, LED_PIN, FX_MODE_RAINBOW_CYCLE,   100,        200,   5000); // Adjust parameters as needed
 
+#define LED_COUNT_SCHWERT 105
+#define LED_COUNT_KAMM 100
+#define LED_COUNT_MAIN 70
+
+#define LED_SCHWERT 7
+#define LED_KAMM 8
+#define LED_MAIN 9
+
+                                                                       //brightness, speed, duration in millis 
+LEDController ledControllerSchwert(LED_COUNT_SCHWERT, LED_SCHWERT, FX_MODE_RAINBOW_CYCLE,   200,        200,   40000); // Adjust parameters as needed
+LEDController ledControllerKamm(LED_COUNT_KAMM, LED_KAMM, FX_MODE_RUNNING_LIGHTS,   200,        200,   40000); // Adjust parameters as needed
+LEDController ledControllerMain(LED_COUNT_MAIN, LED_MAIN, FX_MODE_RUNNING_COLOR,   200,        200,   40000); // Adjust parameters as needed
 void setup() {
-  ledController.init();
-  ledController.start();
+  ledControllerSchwert.init();
+  ledControllerSchwert.start();
+  ledControllerKamm.init();
+  ledControllerKamm.start();
+  ledControllerMain.init();
+  ledControllerMain.start();
 }
 
 void loop() {
-    ledController.service();
+    ledControllerSchwert.service();
+    ledControllerKamm.service();
+    ledControllerMain.service();
 }
