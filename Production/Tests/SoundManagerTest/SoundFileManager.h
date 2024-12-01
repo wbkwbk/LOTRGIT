ae8f9f77e3ff2f7c26faabcb588ef88c90478a28
wbkwbk <80103165+wbkwbk@users.noreply.github.com>
2024-12-01 19:12:31 +0100

    sounds added

--- Production/Tests/SoundManagerTest/SoundFileManager.h
+++ Production/Tests/SoundManagerTest/SoundFileManager.h
@@ -6,39 +6,39 @@
 class SoundFileManager {
   private:
     static const int BALROGHIT_COUNT = 5;
-    static const int BALROGOPENRAMPS_COUNT = 4;
-    static const int BALROGCLOSEDRAMPS_COUNT = 3;
-    static const int BALROGOPENLEFTORBIT_COUNT = 3;
-    static const int BALROGCLOSEDLEFTORBIT_COUNT = 4;
-    static const int BALROGCLOSED_COUNT = 3;
+    static const int BALROGOPENRAMPS_COUNT = 2;
+    static const int BALROGCLOSEDRAMPS_COUNT = 16;
+    static const int BALROGOPENLEFTORBIT_COUNT = 1;
+    static const int BALROGCLOSEDLEFTORBIT_COUNT = 13;
+    static const int BALROGCLOSED_COUNT = 1;
 
     // Arrays für Sounddateien
     const char* balrogHitSounds[BALROGHIT_COUNT] = {
-      "BalrogHit1.mp3", "BalrogHit2.mp3", "BalrogHit3.mp3", "BalrogHit4.mp3", "BalrogHit5.mp3"
+      "ROAR1.mp3",	"GROWL.mp3",	"ROAR2.mp3",	"ROAR3.mp3",	"ROAR5.mp3"
     };
     const char* balrogOpenRampsSounds[BALROGOPENRAMPS_COUNT] = {
-      "BalrogOpenRamp1.mp3", "BalrogOpenRamp2.mp3", "BalrogOpenRamp3.mp3", "BalrogOpenRamp4.mp3"
+      "OTBF.mp3",	"YSNPWE.mp3"
     };
     const char* balrogClosedRampsSounds[BALROGCLOSEDRAMPS_COUNT] = {
-      "BalrogClosedRamp1.mp3", "BalrogClosedRamp2.mp3", "BalrogClosedRamp3.mp3"
+      "AWINL.mp3",	"CWMML.mp3",	"FOAT.mp3",	"MIIJY.mp3",	"PFB.mp3",	"TINBOT.mp3",	"NTRHBHH.mp3",	"SHR.mp3",	"SHAF.mp3",	"TRIATE.mp3",	"WHITF.mp3",	"TRWBS.mp3",	"IBWFGR.mp3",	"IYATOR.mp3",	"WRMRF.mp3",	"BFTRF.mp3"
     };
     const char* balrogOpenLeftOrbitSounds[BALROGOPENLEFTORBIT_COUNT] = {
-      "BalrogOpenLeftOrbit1.mp3", "BalrogOpenLeftOrbit2.mp3", "BalrogOpenLeftOrbit3.mp3"
+      "HOS.mp3"
     };
     const char* balrogClosedLeftOrbitSounds[BALROGCLOSEDLEFTORBIT_COUNT] = {
-      "BalrogClosedLeftOrbit1.mp3", "BalrogClosedLeftOrbit2.mp3", "BalrogClosedLeftOrbit3.mp3", "BalrogClosedLeftOrbit4.mp3"
+      "IWRHNC.mp3",	"SWTB.mp3",	"GNLY.mp3",	"GSLH.mp3",	"GSFH.mp3",	"MPR.mp3",	"MPRES2.mp3",	"ICAML.mp3",	"ORTRTA.mp3",	"WMYMRF.mp3",	"TSDNHS.mp3",	"IAHY.mp3",	"WFOWSE.mp3",
     };
     const char* balrogClosedSounds[BALROGCLOSED_COUNT] = {
-      "BalrogClosed1.mp3", "BalrogClosed2.mp3", "BalrogClosed3.mp3"
+      "GBTTS.mp3"
     };
 
     // Indizes zur Verfolgung der verwendeten Sounds
     bool balrogHitUsed[BALROGHIT_COUNT] = { false, false, false, false, false };
-    bool balrogOpenRampsUsed[BALROGOPENRAMPS_COUNT] = { false, false, false, false };
-    bool balrogClosedRampsUsed[BALROGCLOSEDRAMPS_COUNT] = { false, false, false };
-    bool balrogOpenLeftOrbitUsed[BALROGOPENLEFTORBIT_COUNT] = { false, false, false };
-    bool balrogClosedLeftOrbitUsed[BALROGCLOSEDLEFTORBIT_COUNT] = { false, false, false, false };
-    bool balrogClosedUsed[BALROGCLOSED_COUNT] = { false, false, false };
+    bool balrogOpenRampsUsed[BALROGOPENRAMPS_COUNT] = { false, false};
+    bool balrogClosedRampsUsed[BALROGCLOSEDRAMPS_COUNT] = { false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false };
+    bool balrogOpenLeftOrbitUsed[BALROGOPENLEFTORBIT_COUNT] = { false};
+    bool balrogClosedLeftOrbitUsed[BALROGCLOSEDLEFTORBIT_COUNT] = { false, false, false, false, false , false , false , false , false , false , false , false , false  };
+    bool balrogClosedUsed[BALROGCLOSED_COUNT] = { false};
 
   public:
     // Constructor
